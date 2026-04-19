c6277560bf03c758b03ccf1ec4fc19aa0b88e9ed
Kanipakam-Jeswanth <jeswanth.kanipakam@fau.de>
2026-04-19 17:03:42 +0200

    Clearing all tracked files to reset structure

--- Downloads/ROS2-Bumperbot-master/src/build/bumperbot_motion/ament_cmake_python/bumperbot_motion/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='bumperbot_motion',
-    version='0.0.0',
-    packages=find_packages(
-        include=('bumperbot_motion', 'bumperbot_motion.*')),
-)
--- Downloads/ROS2-Bumperbot-master/src/install/bumperbot_msgs/share/bumperbot_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(bumperbot_msgs_IDL_FILES "action/Fibonacci.idl;srv/AddTwoInts.idl;srv/GetTransform.idl")
-set(bumperbot_msgs_INTERFACE_FILES "action/Fibonacci.action;srv/AddTwoInts.srv;srv/GetTransform.srv")
