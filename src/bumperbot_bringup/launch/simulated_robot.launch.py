25e4fdb071735e954aee15e8a24ed671ca42cbac
Jeswanth_Kanipakam <jeswanthkanipakam@gmail.com>
2026-05-16 16:49:36 +0200

    Integrate navigation and update RViz settings
    Added navigation launch file and updated RViz configuration.

--- src/bumperbot_bringup/launch/simulated_robot.launch.py
+++ src/bumperbot_bringup/launch/simulated_robot.launch.py
@@ -6,14 +6,8 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-#it launches three components:
-#gazebo simulation (gazebo.launch.py)
-#robot controller (controller.launch.py)
-#joystick teleoperation (joystick_teleop.launch.py)
-
 
 def generate_launch_description():
-
     use_slam = LaunchConfiguration("use_slam")
 
     use_slam_arg = DeclareLaunchArgument(
@@ -21,7 +15,6 @@ def generate_launch_description():
         default_value="false"
     )
 
-    # Includes a launch file from bumperbot_description package that starts the Gazebo environment and spawns the robot
     gazebo = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("bumperbot_description"),
@@ -30,7 +23,6 @@ def generate_launch_description():
         ),
     )
     
-    # Includes the robot control logic from the bumperbot_controller package it passes:
     controller = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("bumperbot_controller"),
@@ -38,14 +30,11 @@ def generate_launch_description():
             "controller.launch.py"
         ),
         launch_arguments={
-            #likely selects a more advanced control mode
             "use_simple_controller": "False",
-            #probably disables Python based control nodes, may be preferring C++
             "use_python": "False"
         }.items(),
     )
     
-    # Launches joystick based control for manual operation of the robot. the use_sim_time=True ensures it synchronizes with Gazebo simulated time
     joystick = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("bumperbot_controller"),
@@ -55,13 +44,6 @@ def generate_launch_description():
         launch_arguments={
             "use_sim_time": "True"
         }.items()
-    )
-
-    safety_stop = Node(
-        package="bumperbot_utils",
-        executable="safety_stop",
-        output="screen",
-        parameters=[{"use_sim_time": True}]
     )
 
     localization = IncludeLaunchDescription(
@@ -82,43 +64,34 @@ def generate_launch_description():
         condition=IfCondition(use_slam)
     )
 
-    rviz_localization = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=["-d", os.path.join(
-                get_package_share_directory("bumperbot_localization"),
-                "rviz",
-                "global_localization.rviz"
-            )
-        ],
-        output="screen",
-        parameters=[{"use_sim_time": True}],
-        condition=UnlessCondition(use_slam)
+    navigation = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_navigation"),
+            "launch",
+            "navigation.launch.py"
+        ),
     )
 
-    rviz_slam = Node(
+    rviz = Node(
         package="rviz2",
         executable="rviz2",
         arguments=["-d", os.path.join(
-                get_package_share_directory("bumperbot_mapping"),
+                get_package_share_directory("nav2_bringup"),
                 "rviz",
-                "slam.rviz"
+                "nav2_default_view.rviz"
             )
         ],
         output="screen",
-        parameters=[{"use_sim_time": True}],
-        condition=IfCondition(use_slam)
+        parameters=[{"use_sim_time": True}]
     )
     
-    # Returns LaunchDescription object containing all the included launch files, making this script a convenient single command to bring up simulation, control and teleop
     return LaunchDescription([
         use_slam_arg,
         gazebo,
         controller,
         joystick,
-        safety_stop,
         localization,
         slam,
-        rviz_localization,
-        rviz_slam
+        navigation,
+        rviz,
     ])
